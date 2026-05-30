547f710c590b22a973ac5059a15cc7de2b4cf3e4
hemalathareddi <reddihemalatha67@gmail.com>
2026-05-30 10:36:15 +0530

    second commit

---
+++ function.py
@@ -0,0 +1,63 @@
+def say_hello():
+    print("welcome to python!")
+say_hello()
+
+def add(a,b):
+    return a+b
+result=add(5,3)
+print(result)
+def no_return():
+    print("this function has no return")
+result=no_return()
+print(result)
+def area_of_rectangle(length,width):
+    return length*width
+print(area_of_rectangle(6,4))
+def mul(a,b,c):
+    return a*b*c
+print(mul(6,5,7))
+def pet(animal,name):
+    print(f'my{animal} is named {name}.')
+pet("dog","tom")
+def power(base,exponent):
+    return base**exponent
+print (power(5,2))
+print (power(6,4))
+def full_name(first,middle,last):
+    return first + " " + midle + " " + last
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
