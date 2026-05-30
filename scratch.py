4dc0a9f4e72705792607ab34fed01e4ed8495b58
hemalathareddi <reddihemalatha67@gmail.com>
2026-05-30 11:03:51 +0530

    second commit

---
+++ scratch.py
@@ -0,0 +1,11 @@
+def intro(name,city,hobby):
+    print(f"{name} lives in {city} and likes {hobby}.")
+intro("hema","hyd","hockey")
+intro("delhi","reading","riya")
+def subtract(a,b):
+    return a-b
+print(subtract(10,3))
+print(subtract(3,10))
+def bio(first_name,last_name,age):
+    print(f"name: {first_name}{last_name}, age: {age}")
+bio("amar","sharma",21)
